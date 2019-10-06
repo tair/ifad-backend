e3767163e8ee7e651ce65a4df3c82e7a5073958b
Nick Mosher <nicholastmosher@gmail.com>
2019-10-06 13:35:50 -0400

    Add service endpoint for genes query

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { Server } from 'typescript-rest';
-import { HelloWorldService } from './services/hello_world/service';
+import {V1Service} from "./services/v1_service/service";
 
 const app = express();
 
-Server.buildServices(app, HelloWorldService);
+Server.buildServices(app, V1Service);
 
 export {app};
