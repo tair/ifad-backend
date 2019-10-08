2a17cc87558d246a44b1bf124b03b21744c41266
Nick Mosher <nicholastmosher@gmail.com>
2019-10-08 17:54:55 -0400

    Add dist/ to .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const PORT = process.env.PORT || 3000;
-server_1.app.listen(PORT, () => {
-    console.log(`🚢 Now listening on 0.0.0.0:${PORT} 🔥`);
-});
-//# sourceMappingURL=index.js.map
